43108b98805a4ea7b893a2098fbbefc0a29ca9f6
qingtama <haoxiuqing123@gmail.com>
2018-05-05 21:47:23 +0800

    add memcached

--- resin_test/src/main/java/com/self/FirstServlet.java
+++ resin_test/src/main/java/com/self/FirstServlet.java
@@ -1,11 +1,13 @@
 package com.self;
 
+import net.rubyeye.xmemcached.MemcachedClient;
+import net.rubyeye.xmemcached.XMemcachedClient;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by haoxiuqing on 2017/5/7.
@@ -20,7 +22,23 @@ public class FirstServlet extends HttpServlet {
         // 实际的逻辑是在这里
         //PrintWriter out = response.getWriter();
         //out.println("<h1>" + 123 + "</h1>");
+        dataInMemcache("test");
+
         long end = System.currentTimeMillis();
         System.out.println(end-start);
+    }
+
+    private void dataInMemcache(String key){
+        try {
+            MemcachedClient client = new XMemcachedClient("host", 11211);
+
+            //store a value for one hour(synchronously).
+            client.set("key", 3600,  key);
+            //Retrieve a value.(synchronously).
+            String value = client.get("key");
+            System.out.println("memcached:" + value);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
